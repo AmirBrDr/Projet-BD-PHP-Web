b0c3d969fc06487f2cf8cf5fa592e61da1df7217
Romeo andriantsiferana <romeo.andriant@gmail.com>
2026-05-04 15:25:45 +0200

    ajout }

--- public/assets/js/compteRH.js
+++ public/assets/js/compteRH.js
@@ -63,6 +63,8 @@ class CompteRHManager {
             }
         }
 
+    }
+
     saveAll() {
         localStorage.setItem(this.storageKeys.profile, JSON.stringify(this.user));
         localStorage.setItem(this.storageKeys.security, JSON.stringify(this.securityInfo));
