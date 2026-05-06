68e083a01ce142b91c1260398e75362042c37b93
NicolasRuiz2005 <nicolas.ruiz2075@gmail.com>
2026-05-06 18:59:50 +0200

    Première avancée modif anim et admin 06/05

---
+++ db/seed_appartenir.sql
@@ -0,0 +1,19 @@
+-- 1 mobilité
+-- 2 Energie
+-- 3 Déchets
+-- 4 Alimentation
+
+
+
+insert into appartenir (id_defi, id_thematique) values (1, 1);
+insert into appartenir (id_defi, id_thematique) values (2, 1);
+insert into appartenir (id_defi, id_thematique) values (3, 1);
+insert into appartenir (id_defi, id_thematique) values (4, 2);
+insert into appartenir (id_defi, id_thematique) values (5, 2);
+insert into appartenir (id_defi, id_thematique) values (6, 2);
+insert into appartenir (id_defi, id_thematique) values (7, 3);
+insert into appartenir (id_defi, id_thematique) values (8, 3);
+insert into appartenir (id_defi, id_thematique) values (9, 3);
+insert into appartenir (id_defi, id_thematique) values (10, 4);
+insert into appartenir (id_defi, id_thematique) values (11, 4);
+insert into appartenir (id_defi, id_thematique) values (12, 4);
